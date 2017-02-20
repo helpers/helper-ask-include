56e1be1a0c74b3e59b7e2bdf08b51e5eeb78b57e
jonschlinkert <jon.schlinkert@sellside.com>
2017-02-20 02:31:45 -0500

    move examples

--- examples/templates.js
+++ examples/templates.js
@@ -6,16 +6,20 @@ var app = templates();
 app.use(questions());
 
 app.engine('md', require('engine-base'));
-app.asyncHelper('askInclude', require('./')(app));
+app.asyncHelper('askInclude', require('..'));
 
 app.create('pages');
-app.create('includes');
+app.create('includes', {viewType: 'partial'});
+app.create('layouts', {viewType: 'layout'});
+
+app.layout('default', {content: 'Above\n{% body %}\nBelow'});
 
 app.include('a.md', {content: 'this is AAA'});
 app.include('b.md', {content: 'this is BBB'});
 app.include('c.md', {content: 'this is CCC'});
 
 var index = app.page('index.md', {
+  layout: 'default',
   content: 'Before\n<%= askInclude("includes") %>\nAfter'
 });
 
